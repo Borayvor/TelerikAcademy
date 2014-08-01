757aa5545a3442f7a3197110d797d967e1dbb3b7
Boray <boray@abv.bg>
2014-08-01 21:22:56 +0300

    Exam_01_07_Dec_2011_Morning

--- Exam_01_07_Dec_2011_Morning/Problem04DancingBits/DancingBits.cs
+++ Exam_01_07_Dec_2011_Morning/Problem04DancingBits/DancingBits.cs
@@ -7,7 +7,7 @@
         public static void Main(string[] args)
         {
             string sequenceOfBits = null;
-            int count = 0;            
+            int countEqualBits = 0;            
 
             int K = int.Parse(Console.ReadLine());
 
@@ -20,32 +20,32 @@
                         
             for(int index = 0; index < sequenceOfBits.Length; index++ )
             {
-                int localCount = 0;
+                int countDancingBits = 0;
 
-                for(int indexNext = index; indexNext < sequenceOfBits.Length; indexNext++)
+                for(int indexDancingBits = index; indexDancingBits < sequenceOfBits.Length; indexDancingBits++)
                 {         
-                    if(sequenceOfBits[index] != sequenceOfBits[indexNext])
+                    if(sequenceOfBits[index] != sequenceOfBits[indexDancingBits])
                     {
-                        index = indexNext - 1;
+                        index = indexDancingBits - 1;
                         break;
                     }
 
-                    localCount++;
+                    countDancingBits++;
 
-                    if(indexNext == sequenceOfBits.Length - 1)
+                    if(indexDancingBits == sequenceOfBits.Length - 1)
                     {
-                        index = indexNext;
+                        index = indexDancingBits;
                         break;
                     }
                 }
 
-                if(localCount == K)
+                if(countDancingBits == K)
                 {
-                    count++;
+                    countEqualBits++;
                 }
             } 
 
-            Console.WriteLine(count);
+            Console.WriteLine(countEqualBits);
         }
     }
 }
